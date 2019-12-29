5e2977e8d3068ef484d17dbd4d6ab72f8eb5134e
teratorn <teratorn@teratorn>
2019-12-29 01:00:22 -0600

    also support fetching/installing PyCharm, Git, and TortoiseGit

--- get_tools.bat
+++ get_tools.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: skip downloading/setting-up python if we already have python3
-python -c "import sys; sys.exit(int(not sys.version_info[0] >= 3))" > NUL 2> NUL && goto :get_inno
+python -c "import sys; sys.exit(int(not sys.version_info[0] >= 3))" > NUL 2> NUL && goto :skip_get_python
 
 echo Press any key to launch your browser and download python-3.7.6.exe
 echo.
@@ -38,12 +38,16 @@ for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpa
 
 :: Set Refreshed Path
 set PATH=%userpath%;%syspath%
+goto :get_inno
 
+:skip_get_python
+echo Looks like you already have Python 3 on your system, skipping this step...
+pause
 
 :get_inno
 
 :: skip downloading/setting-up innosetup if we already have it
-reg query "HKCR\InnoSetupScriptFile\shell\Compile\command" >NUL 2>NUL && goto :get_py_modules
+reg query "HKCR\InnoSetupScriptFile\shell\Compile\command" >NUL 2>NUL && goto :skip_get_inno
 
 cls
 echo Press any key to launch your browser and download innosetup
@@ -68,6 +72,12 @@ echo If prompted by a security warning click the "Run" button.
 timeout /t 2 /nobreak > NUL
 
 powershell -c "$inno = Get-ChildItem $HOME\Downloads\innosetup*.exe | Sort-Object -Descending | Select-Object -First 1 -ExpandProperty Name; Start-Process $HOME\Downloads\$inno -Wait -ArgumentList \""/SILENT\"",\""/SUPPRESSMSGBOXES\"",\""/CURRENTUSER\"",\""/NOCANCEL\"",\""/NORESTART\"",\""/NOCLOSEAPPLICATIONS\"",\""/NORESTARTAPPLICATIONS\"",\""/LANG=english\""" || goto :error
+goto :get_py_modules
+
+
+:skip_get_inno
+echo Looks like you already have InnoSetup on your system - skipping this step...
+pause
 
 
 :get_py_modules
@@ -84,7 +94,134 @@ python -m pip install --upgrade pip || goto :error
 pip3 install pyside2 pyinstaller || goto :error
 
 echo.
-echo SUCCESSFULLY INSTALLED EVERYTHING
+echo Successfully downloaded and installed the necessary python modules
+pause
+
+::echo.
+::echo SUCCESSFULLY INSTALLED EVERYTHING NEEDED FOR EngUtils DEVELOPMENT
+::pause
+::
+::cls
+::echo You can close this window now if you wish. If you continue we'll download
+::echo a few optional things that make EngUtils development easier.
+::echo.
+::pause
+
+
+:: skip downloading/setting-up PyCharm if we already have it
+python -c "import winreg;winreg.EnumKey(winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\JetBrains\\PyCharm Community Edition'), 0)" >NUL 2>NUL && goto :skip_get_pycharm
+
+cls
+echo Press any key to launch your browser and download PyCharm Community Edition
+echo.
+echo Accept this download and save the file to the normal Downloads folder
+echo.
+echo Then switch back to this window to continue the setup process...
+echo.
+pause
+
+start "" "https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=windows&code=PCC" || goto :error
+timeout /t 2 /nobreak > NUL
+echo.
+echo Now ONLY AFTER the download has completed successfully...
+echo.
+pause
+
+echo.
+echo Attempting to run the PyCharm setup program with appropriate options...
+echo.
+echo If prompted by a security warning click the "Run" or "Yes" button.
+echo.
+echo The installer WILL NOT display a progress window, and may take up to a minute to complete.
+echo Please be patient...
+timeout /t 2 /nobreak > NUL
+
+powershell -c "$exe = Get-ChildItem $HOME\Downloads\pycharm*.exe | Sort-Object -Descending | Select-Object -First 1 -ExpandProperty Name; Start-Process $HOME\Downloads\$exe -Wait -ArgumentList \""/S\"",\""/CONFIG=pycharm_silent.config\""" || goto :error
+goto :get_git_for_windows
+
+
+:skip_get_pycharm
+echo Looks like you already have PyCharm on your system - skipping this step...
+pause
+
+
+:get_git_for_windows
+
+:: skip downloading/setting-up Git for Windows if we already have it
+git --help >NUL 2>NUL && goto :skip_get_git_for_windows
+
+cls
+echo Press any key to launch your browser and download Git for Windows
+echo.
+echo Accept this download and save the file to the normal Downloads folder
+echo.
+echo Then switch back to this window to continue the setup process...
+echo.
+pause
+
+start "" "https://git-scm.com/download/win"
+timeout /t 2 /nobreak > NUL
+echo.
+echo Now ONLY AFTER the download has completed successfully...
+echo.
+pause
+
+echo.
+echo Attempting to run the Git for Windows setup program with appropriate options...
+echo.
+echo If prompted by a security warning click the "Run" or "Yes" button.
+echo.
+timeout /t 2 /nobreak > NUL
+
+powershell -c "$exe = Get-ChildItem $HOME\Downloads\Git*.exe | Sort-Object -Descending | Select-Object -First 1 -ExpandProperty Name; Start-Process $HOME\Downloads\$exe -Wait -ArgumentList \""/SP-\"",\""/SILENT\"",\""/SUPPRESSMSGBOXES\"",\""/NOCANCEL\"",\""/NORESTART\"",\""/CLOSEAPPLICATIONS\"",\""/RESTARTAPPLICATIONS\"",\""/LOADINF=git_setup.inf\""" || goto :error
+
+goto :get_tortoisegit
+
+:skip_get_git_for_windows
+echo Looks like you already have Git for Windows on your system - skipping this step...
+pause
+
+
+:get_tortoisegit
+
+:: skip downloading/setting-up TortoiseGit if we already have it
+python -c "import winreg, sys, os;sys.exit(int(not os.path.exists(winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\TortoiseGit'), 'SSH')[0])))" >NUL 2>NUL && goto :skip_get_tortoisegit
+
+cls
+echo Press any key to launch your browser and download TortoiseGit
+echo.
+echo Accept this download and save the file to the normal Downloads folder
+echo.
+echo Then switch back to this window to continue the setup process...
+echo.
+pause
+
+start "" "https://download.tortoisegit.org/tgit/2.9.0.0/TortoiseGit-2.9.0.0-32bit.msi" || goto :error
+timeout /t 2 /nobreak > NUL
+echo.
+echo Now ONLY AFTER the download has completed successfully...
+echo.
+pause
+
+echo.
+echo Attempting to run the TortoiseGit setup program with appropriate options...
+echo.
+echo If prompted by a security warning click the "Run" or "Yes" button.
+echo.
+timeout /t 2 /nobreak > NUL
+
+powershell -c "$exe = Get-ChildItem $HOME\Downloads\TortoiseGit*.msi | Sort-Object -Descending | Select-Object -First 1 -ExpandProperty Name; Start-Process $HOME\Downloads\$exe -Wait -ArgumentList \""/passive\""" || goto :error
+
+goto :done
+
+:skip_get_tortoisegit
+echo Looks like you already have TortoiseGit on your system - skipping this step...
+pause
+
+:done
+echo.
+echo.
+echo ALL DONE!
 pause
 exit
 
